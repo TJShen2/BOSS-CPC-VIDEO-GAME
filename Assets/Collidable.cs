1bc3ca3a106d534197e6f7643b06dab60b6d16c4
Sean-Chen71 <125089755+SeanChen9113@users.noreply.github.com>
2023-04-12 18:35:39 -0400

    Added Controls and Collision

---
+++ Assets/Collidable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    public GameObject particles;
+
+    //This method will run when the player collides with something
+    void OnCollisionEnter2D(Collision2D col) {
+        
+        if (col.gameObject.tag == "Kill Block") {
+
+            //Spawn the kill particles at the collision site
+            GameObject deathParticles = Instantiate(particles) as GameObject;
+            deathParticles.transform.position = col.GetContact(0).point;
+
+            //Set the colour of the particles to the player's colour
+            ParticleSystem dpSystem = deathParticles.GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule dpMain = dpSystem.main;
+            dpMain.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+
+            gameObject.SetActive(false);
+
+            //Destroy the particles after 2 seconds
+            Destroy(deathParticles, 2);
+        }
+    }
+}
--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     public float moveSpeed;
+    public KeyCode boostKey = KeyCode.Space;
+    public KeyCode slowKey = KeyCode.LeftShift;
+
+    private bool canMove;
+    private bool canBoost; 
 
     private Rigidbody2D rb2d;   
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
+        canBoost = true;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(Input.GetKey(boostKey) && canBoost)
+        {
+            rb2d.velocity = Vector2.zero; 
+            canMove = false;
+        }
+
+        if(Input.GetKeyUp(boostKey) && canBoost)
+        {
+            canMove = true;
+            StartCoroutine(boost());
+        }
+
+        if(Input.GetKey(slowKey))
+        {
+            rb2d.drag = 1f;
+        }
+
+        else
+            rb2d.drag = 0f;
+
     }
 
     // Called every fixed timestep
@@ -28,7 +55,16 @@ public class PlayerContoller : MonoBehaviour
 
         transform.up = dirNormalized;
 
-        rb2d.AddForce(dirNormalized * moveSpeed * Time.deltaTime, ForceMode2D.Force);
+        if(canMove)
+            rb2d.AddForce(transform.up * moveSpeed * Time.deltaTime, ForceMode2D.Force);
 
+    }
+
+    IEnumerator boost ()
+    {
+        canBoost = false; 
+        rb2d.AddForce(transform.up * moveSpeed * Time.deltaTime * 50, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(5f);
+        canBoost = true;
     }
 }
